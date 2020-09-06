19bfd9f49c0b5a24f7f2a3694f5673f535b24803
TobiasPiontek <tobias_piontek@gmx.de>
2020-09-06 15:17:16 +0200

    small frontend changes2

--- Frontend/scripts/leafletscript.js
+++ Frontend/scripts/leafletscript.js
@@ -38,7 +38,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'mapbox.streets'
+    id: 'mapbox.light'
 }).addTo(mymap);
 
 //function to test marker functionality
@@ -61,7 +61,7 @@ mymap.on('click', function(e) {
 		checkFirst();
 		compute();
 		next();
-		
+
     } else {
         if (secondmarker) {
             mymap.removeLayer(secondmarker);
