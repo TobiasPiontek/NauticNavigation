b51aa96ae0b69bd0f322bc144f2994494a316de3
TobiasPiontek <tobias_piontek@gmx.de>
2020-09-18 20:24:42 +0200

    added Benchmark files

--- src/MoogleGaps/Benckmark.java
+++ src/MoogleGaps/Benckmark.java
@@ -10,7 +10,7 @@ public class Benckmark {
 
     private static int[] dijkstraPulls;
     private static long[] dijkstraTimeConsumption;
-    private static boolean[] dijkstrafoundWay;
+    private static boolean[] dijkstraFoundWay;
 
     private static int[] aStarPulls;
     private static long[] aStarTimeConsumption;
@@ -32,7 +32,7 @@ public class Benckmark {
         //init Benchmarking variables
         dijkstraPulls = new int[sampleSize];
         dijkstraTimeConsumption = new long[sampleSize];
-        dijkstrafoundWay = new boolean[sampleSize];
+        dijkstraFoundWay = new boolean[sampleSize];
 
         aStarPulls = new int[sampleSize];
         aStarTimeConsumption = new long[sampleSize];
@@ -52,7 +52,7 @@ public class Benckmark {
     private static void calculateDijkstraBenchmark(int[] startNodes, int[] endNodes) {
         for (int i = 0; i < startNodes.length; i++) {
             long startTime = System.nanoTime();
-            dijkstrafoundWay[i] = !Navigation.dijkstra(startNodes[i], endNodes[i]).isEmpty();
+            dijkstraFoundWay[i] = !Navigation.dijkstra(startNodes[i], endNodes[i]).isEmpty();
             long endTime = System.nanoTime();
             dijkstraTimeConsumption[i] = endTime - startTime;
             dijkstraPulls[i] = Navigation.pulls;
@@ -78,9 +78,8 @@ public class Benckmark {
                 double latitude = Math.random() * 170.0 - 80.0;
                 double longitude = Math.random() * 360.0 - 180.0;
                 nodeIdGridGraph = GridGraph.findVertex(longitude, latitude);
-            } while (/*!Navigation.isSurroundedByWater(nodeIdGridGraph) && */GridGraph.vertexData[nodeIdGridGraph]);
+            } while (GridGraph.vertexData[nodeIdGridGraph]);
             nodesInWater[i] = nodeIdGridGraph;
-            //System.out.println("Debug: longitude: " + GridGraph.idToLongitude(nodeIdGridGraph) + ", latitude: " + GridGraph.idToLatitude(nodeIdGridGraph));
         }
         return nodesInWater.clone();
     }
@@ -88,18 +87,18 @@ public class Benckmark {
 
     private static void writeDataToCsv(String filepath) throws FileNotFoundException {
         PrintWriter out = new PrintWriter(filepath);
-        out.println("start lat, start long, dest lat, dest long, dijkstra time, astar time, dijsktra node pulls,  astar node pulls, dijkstra found, astar found");
+        out.println("start lat, start long, dest lat, dest long, dijkstra time, astar time, dijsktra node pulls,  astar node pulls, dijkstra found, astar found, index");
         double dijkstraTimeTotal = 0;
         double dijkstraTimeFound = 0;
-        double astarTimeTotal = 0;
-        double astarTimeFound = 0;
+        double aStarTimeTotal = 0;
+        double aStarTimeFound = 0;
         long dijkstraPullsTotal = 0;
         long dijkstraPullsFound = 0;
         long aStarPullsTotal = 0;
         long aStarPullsFound = 0;
         int dijkstraFound = 0;
-        int astarFound = 0;
-
+        int aStarFound = 0;
+        
         for (int i = 0; i < dijkstraPulls.length; i++) {
             out.println(
                     GridGraph.idToLatitude(startNodes[i]) + ","
@@ -110,49 +109,50 @@ public class Benckmark {
                             + aStarTimeConsumption[i] / Math.pow(10, 9) + ","
                             + dijkstraPulls[i] + ","
                             + aStarPulls[i] + ","
-                            + dijkstrafoundWay[i] + ","
-                            + aStarFoundWay[i]);
+                            + dijkstraFoundWay[i] + ","
+                            + aStarFoundWay[i] + ","
+                            + i);
 
             dijkstraTimeTotal += dijkstraTimeConsumption[i] / Math.pow(10, 9);
-            astarTimeTotal += aStarTimeConsumption[i] / Math.pow(10, 9);
+            aStarTimeTotal += aStarTimeConsumption[i] / Math.pow(10, 9);
             dijkstraPullsTotal += dijkstraPulls[i];
             aStarPullsTotal += aStarPulls[i];
 
             //calculate only routes that have a path
-            if (dijkstrafoundWay[i]) {
+            if (dijkstraFoundWay[i]) {
                 dijkstraTimeFound += dijkstraTimeConsumption[i] / Math.pow(10, 9);
                 dijkstraPullsFound += dijkstraPulls[i];
                 dijkstraFound++;
             }
             if (aStarFoundWay[i]) {
-                astarTimeFound += aStarTimeConsumption[i] / Math.pow(10, 9);
+                aStarTimeFound += aStarTimeConsumption[i] / Math.pow(10, 9);
                 aStarPullsFound += aStarPulls[i];
-                astarFound++;
+                aStarFound++;
             }
         }
         out.println();
         out.println("Analysis");
         out.println(",Time speedup, Nodes used less,,t dijkstra avg.,t astar avg., total dijkstra, total astar, dijkstra routes, astar routes");
         out.println("Average all" + ","
-                + dijkstraTimeTotal / astarTimeTotal + ","
+                + dijkstraTimeTotal / aStarTimeTotal + ","
                 + (double) aStarPullsTotal / dijkstraPullsTotal + "," + ","
                 + dijkstraTimeTotal / dijkstraPulls.length + ","
-                + astarTimeTotal / dijkstraPulls.length + ","
+                + aStarTimeTotal / dijkstraPulls.length + ","
                 + dijkstraPullsTotal / dijkstraPulls.length + ","
                 + aStarPullsTotal / dijkstraPulls.length + ","
                 + dijkstraPulls.length + ","
-                + dijkstraPulls.length);
+                + dijkstraPulls.length + ",");
 
         out.println();
         out.println("Average found" + ","
-                + dijkstraTimeFound / astarTimeFound + ","
+                + dijkstraTimeFound / aStarTimeFound + ","
                 + (double) aStarPullsFound / dijkstraPullsFound + "," + ","
                 + dijkstraTimeFound / dijkstraFound + ","
-                + astarTimeFound / astarFound + ","
+                + aStarTimeFound / aStarFound + ","
                 + dijkstraPullsFound / dijkstraFound + ","
-                + aStarPullsFound / astarFound + ","
+                + aStarPullsFound / aStarFound + ","
                 + dijkstraFound + ","
-                + astarFound
+                + aStarFound
         );
         out.println();
         out.close();
